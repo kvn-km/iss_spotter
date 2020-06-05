2aa408e0e748329909909b8e5e7f96167bb57ff3
Kevin Kim <kevinfkim@gmail.com>
2020-06-05 01:45:29 +0000

    finally got the coords function/callback to wrk.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
-//     console.log("It didn't work!", error);
+//     console.log("IP didn't work!", error);
 //     return;
 //   }
 //   console.log('It worked! Returned IP:', ip);
+// });
+
+// fetchCoordsByIP("99.235.186.103", (error, geo) => {
+//   if (error) {
+//     console.log("Geo didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned GeoCoords:', geo);
 // });
--- iss.js
+++ iss.js
@@ -1,32 +1,34 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
-
 const request = require("request");
 
 const fetchMyIP = function(callback) {
-
   request("https://api.ipify.org?format=json", (error, response, body) => {
-
     if (error) return callback(error, null);
-
     if (response.statusCode !== 200) {
       callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
       return;
     }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
 
-    callback(null, JSON.parse(body).ip);
+const fetchCoordsByIP = function(ip, callback) {
 
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching geoCoords by IP. Response: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
   });
 
 };
 
-// fetchMyIP();
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
 
